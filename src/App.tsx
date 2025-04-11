bc870e17658ff4cb8a66513ada5439a1d595a03e
Max Gemereth <177759368+gemerethmax@users.noreply.github.com>
2025-04-11 10:12:25 -0400

    Edit Post component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
 import { SinglePostPage } from './features/posts/SinglePostPage'
 import {PostsMainPage} from './features/posts/PostsMainPage'
-
+import { EditPostForm } from './features/posts/EditPostForm'
 function App() {
   return (
     <Router>
@@ -12,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<PostsMainPage />}></Route>
           <Route path="/posts/:postId" element={<SinglePostPage />}></Route>
+          <Route path="/editPost/:postId" element={<EditPostForm />} />
         </Routes>
       </div>
     </Router>
