de1302763279c5ddbb6fdae875e88b8a45adbe92
Max Gemereth <177759368+gemerethmax@users.noreply.github.com>
2025-04-15 08:28:45 -0400

    Sort posts list in reverse-chronological order

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -2,17 +2,21 @@ import { useAppSelector } from '../../app/hooks'
 import { Link } from 'react-router-dom'
 import { selectAllPosts } from './postsSlice'
 import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from '@/components/TimeAgo'
 
 export const PostsList = () => {
     const posts = useAppSelector(selectAllPosts)
 
-    const renderedPosts = posts.map(post => (
+
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+    const renderedPosts = orderedPosts.map((post) => (
         <article className="post-excerpt" key={post.id}>
             <h3>
                <Link to={`/posts/${post.id}`}>{post.title}</Link> 
             </h3>
             <div>
                 <PostAuthor userId={post.user} />
+                <TimeAgo timestamp={post.date} />
             </div>
             <p className="post-content">{post.content.substring(0, 100)}</p>
             
