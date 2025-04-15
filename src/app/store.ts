8c337f325418e2f2b18a5a8010603ec6efeb96c6
Max Gemereth <177759368+gemerethmax@users.noreply.github.com>
2025-04-15 10:10:46 -0400

    authSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,12 +2,14 @@ import {configureStore} from '@reduxjs/toolkit';
 import type {Action} from '@reduxjs/toolkit';
 import postsReducer from '../features/posts/postsSlice';
 import usersReducer from '../features/users/usersSlice';
+import authReducer from '../features/auth/authSlice';
 
 
 export const store = configureStore({
     reducer: {
         posts: postsReducer,
-        users: usersReducer
+        users: usersReducer,
+        auth: authReducer
     }
 });
 
