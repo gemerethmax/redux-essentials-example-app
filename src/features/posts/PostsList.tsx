452efde5962d904b5d04f29768909fb344c40366
Max Gemereth <177759368+gemerethmax@users.noreply.github.com>
2025-04-22 09:28:29 -0400

    Show loading/error states while fetching posts

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -1,7 +1,8 @@
 import { useAppSelector, useAppDispatch } from '@/app/hooks'
 import { Link } from 'react-router-dom'
-import { Post, selectAllPosts, fetchPosts, selectPostsStatus } from './postsSlice'
+import { Post, selectAllPosts, fetchPosts, selectPostsStatus, selectPostsError } from './postsSlice'
 import { PostAuthor } from './PostAuthor'
+import { Spinner } from '@/components/Spinner'
 import { TimeAgo } from '@/components/TimeAgo'
 import { ReactionButtons } from './ReactionButtons'
 import React, { useEffect} from 'react'
@@ -31,6 +32,8 @@ export const PostsList = () => {
     const dispatch = useAppDispatch()
     const posts = useAppSelector(selectAllPosts)
     const postStatus = useAppSelector(selectPostsStatus)
+    const postsError = useAppSelector(selectPostsError)
+
 
     useEffect(() => {
         if (postStatus === 'idle') {
@@ -38,16 +41,24 @@ export const PostsList = () => {
         }
     }, [postStatus, dispatch])
 
-    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
-
-    const renderedPosts = orderedPosts.map((post) => (
-        <PostExcerpt key={post.id} post={post} />)
-    )
-
+    let content: React.ReactNode
+ 
+    if (postStatus === 'pending') {
+      content = <Spinner text="Loading..." />
+    } else if (postStatus === 'succeeded') {
+      // Sort posts in reverse chronological order by datetime string
+      const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+  
+    
+      content = orderedPosts.map((post) => <PostExcerpt key={post.id} post={post} />)
+    } else if (postStatus === 'rejected') {
+      content = <div>{postsError}</div>
+    }
+  
     return (
-        <section className="posts-list">
-            <h2>Posts</h2>
-            {renderedPosts}
-        </section>
+      <section className="posts-list">
+        <h2>Posts</h2>
+        {content}
+      </section>
     )
-}
+  }
