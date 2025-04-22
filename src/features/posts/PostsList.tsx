2a30329c5b549e26d966055d29a2d405ae783fd1
Max Gemereth <177759368+gemerethmax@users.noreply.github.com>
2025-04-22 08:59:22 -0400

    Extract PostExcerpt component

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -1,10 +1,32 @@
 import { useAppSelector, useAppDispatch } from '@/app/hooks'
 import { Link } from 'react-router-dom'
-import { selectAllPosts, fetchPosts, selectPostsStatus } from './postsSlice'
+import { Post, selectAllPosts, fetchPosts, selectPostsStatus } from './postsSlice'
 import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from '@/components/TimeAgo'
 import { ReactionButtons } from './ReactionButtons'
 import React, { useEffect} from 'react'
+
+interface PostsExcerptProps {
+    post: Post
+}
+
+function PostExcerpt({ post }: PostExcerptProps) {
+    return (
+        <article className="post-excerpt" key={post.id}>
+            <h3>
+                <Link to={`/posts/${post.id}`}>{post.title}</Link>
+            </h3>
+            <div>
+                <PostAuthor userId={post.user} />
+                <TimeAgo timestamp={post.date} />
+            </div>
+            <p className="post-content">{post.content.substring(0, 100)}
+         </p>
+            <ReactionButtons post={post} />
+        </article>
+    )
+}
+
 export const PostsList = () => {
     const dispatch = useAppDispatch()
     const posts = useAppSelector(selectAllPosts)
@@ -17,20 +39,10 @@ export const PostsList = () => {
     }, [postStatus, dispatch])
 
     const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+
     const renderedPosts = orderedPosts.map((post) => (
-        <article className="post-excerpt" key={post.id}>
-            <h3>
-               <Link to={`/posts/${post.id}`}>{post.title}</Link> 
-            </h3>
-            <div>
-                <PostAuthor userId={post.user} />
-                <TimeAgo timestamp={post.date} />
-            </div>
-            <p className="post-content">{post.content.substring(0, 100)}</p>
-            <ReactionButtons post={post} />
-            
-        </article>
-    ))
+        <PostExcerpt key={post.id} post={post} />)
+    )
 
     return (
         <section className="posts-list">
